7ec41b87a82d27ee13f50862a82c9a34d29e20af
Ben Durrant <ben.durrant@marketdojo.com>
2025-11-08 23:17:17 +0000

    burn it down

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   plugins: [
@@ -10,7 +9,6 @@ const config = defineConfig({
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-    tailwindcss(),
     tanstackStart(),
     viteReact(),
   ],
