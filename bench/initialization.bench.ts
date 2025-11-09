20b357bbe45fa0ae06e836f9ece6f88e25f8b3a2
Ben Durrant <ben.durrant@marketdojo.com>
2025-11-09 22:35:14 +0000

    test jitless zod

--- bench/initialization.bench.ts
+++ bench/initialization.bench.ts
@@ -1,16 +1,21 @@
-import { describe, bench } from "vitest";
+import { bench } from "vitest";
 import { getArkTypeSchema } from "./schemas/arktype";
 import { getValibotSchema } from "./schemas/valibot";
 import { getZodSchema } from "./schemas/zod";
+import z from "zod";
 
-describe("Initialization", () => {
-  bench("arktype", () => {
-    getArkTypeSchema();
-  });
-  bench("valibot", () => {
-    getValibotSchema();
-  });
-  bench("zod", () => {
+bench("arktype", () => {
+  getArkTypeSchema();
+});
+
+bench("valibot", () => {
+  getValibotSchema();
+});
+
+for (const jitless of [false, true]) {
+  z.config({ jitless });
+  bench("zod" + (jitless ? " (jitless)" : ""), () => {
     getZodSchema();
   });
-});
+}
+z.config({ jitless: false });
--- bench/validation.bench.ts
+++ bench/validation.bench.ts
@@ -6,27 +6,34 @@ import { getValibotSchema } from "./schemas/valibot";
 import { getZodSchema } from "./schemas/zod";
 import { TypiaSchema } from "./schemas/typia";
 import { successData, errorData } from "@schema-benchmarks/data";
+import z from "zod";
 
-describe("Validation", () => {
-  const arktypeSchema = getArkTypeSchema();
-  const valibotSchema = getValibotSchema();
-  const zodSchema = getZodSchema();
+const arktypeSchema = getArkTypeSchema();
 
-  describe.each([
-    ["Success", successData],
-    ["Error", errorData],
-  ])("%s", (_, data) => {
-    bench("arktype", () => {
-      arktypeSchema(data);
-    });
-    bench("valibot", () => {
-      v.safeParse(valibotSchema, data);
-    });
-    bench("zod", () => {
+const valibotSchema = getValibotSchema();
+
+describe.each([
+  ["Success", successData],
+  ["Error", errorData],
+])("%s", (_, data) => {
+  bench("arktype", () => {
+    arktypeSchema(data);
+  });
+
+  bench("valibot", () => {
+    v.safeParse(valibotSchema, data);
+  });
+
+  for (const jitless of [false, true]) {
+    z.config({ jitless });
+    const zodSchema = getZodSchema();
+    bench("zod" + (jitless ? " (jitless)" : ""), () => {
       zodSchema.safeParse(data);
     });
-    bench("typia", () => {
-      typia.validate<TypiaSchema>(data);
-    });
+  }
+  z.config({ jitless: false });
+
+  bench("typia", () => {
+    typia.validate<TypiaSchema>(data);
   });
 });
