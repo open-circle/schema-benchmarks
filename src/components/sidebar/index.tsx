9c7434d72f14cb5170b5f57e2533ad2a896cddf2
Ben Durrant <ben.durrant@marketdojo.com>
2025-11-09 12:10:00 +0000

    tweak typography system

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -3,7 +3,7 @@ export function Sidebar() {
 		<aside className="sidebar">
 			<div className="logo">
 				<img src="/logo.svg" alt="Logo" />
-				<h2>Schema Benchmarks</h2>
+				<h2 className="subtitle1">Schema Benchmarks</h2>
 			</div>
 		</aside>
 	);
