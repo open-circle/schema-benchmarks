465e1322b2eb4f9024a1044bbd702422c45b881b
Ben Durrant <ben.durrant@marketdojo.com>
2025-11-11 23:24:26 +0000

    add animation story

---
+++ website/src/styles/animation.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import "./animation.stories.css";
+
+const curves = ["deceleration", "acceleration", "standard", "sharp"] as const;
+type Curve = (typeof curves)[number];
+
+const meta = {
+	title: "Theme/Animation",
+	render: ({ curve }) => (
+		<div className="animation-container">
+			<div
+				className="animation-box card"
+				style={{ animationTimingFunction: `var(--${curve}-curve)` }}
+			>
+				{curve}
+			</div>
+		</div>
+	),
+	argTypes: {
+		curve: {
+			control: {
+				type: "radio",
+			},
+			options: curves,
+		},
+	},
+	args: {
+		curve: "standard",
+	},
+} satisfies Meta<{ curve: Curve }>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
