37658e6f41474e534f03c400de1b9d2986f0ed74
Ben Durrant <ben.durrant@marketdojo.com>
2025-11-11 13:18:07 +0000

    use real data for story

--- website/src/components/results-table/index.stories.tsx
+++ website/src/components/results-table/index.stories.tsx
@@ -1,44 +1,12 @@
+import results from "@schema-benchmarks/bench/results.json";
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import ts from "dedent";
 import { ResultsTable } from ".";
 
 const meta = {
 	title: "Components/ResultsTable",
 	component: ResultsTable,
 	args: {
-		results: [
-			{
-				libraryName: "ajv",
-				note: "validate",
-				rank: 1,
-				snippet: ts`ajv.validate(schema, data)`,
-			},
-			{
-				libraryName: "ajv",
-				note: "compile",
-				rank: 2,
-				snippet: ts`
-					// const validate = ajv.compile(schema);
-					validate(data);
-				`,
-			},
-			{ libraryName: "valibot", rank: 3, snippet: ts`v.is(schema, data)` },
-			{ libraryName: "arktype", rank: 4, snippet: ts`schema.allows(data)` },
-			{
-				libraryName: "effect",
-				rank: 5,
-				snippet: ts`
-					// const is = Schema.is(schema);
-					is(data);
-				`,
-			},
-			{
-				libraryName: "typebox",
-				rank: 6,
-				snippet: ts`Value.Check(schema, data)`,
-			},
-			{ libraryName: "yup", rank: 7, snippet: ts`schema.isValidSync(data)` },
-		],
+		results: results.validation.runtime.error,
 	},
 } satisfies Meta<typeof ResultsTable>;
 
