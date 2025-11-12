fbf2d58d317135dbf9b5f0a0fe255aaf81791f2d
Ben Durrant <ben.durrant@marketdojo.com>
2025-11-12 00:26:07 +0000

    inline crumbs selection

--- website/src/components/header/index.tsx
+++ website/src/components/header/index.tsx
@@ -1,6 +1,5 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useMatches } from "@tanstack/react-router";
 import { Fragment, type ReactNode } from "react";
-import { useCrumbs } from "@/hooks/use-crumbs";
 import { MdSymbol } from "../symbol";
 
 export function Header({ children }: { children: ReactNode }) {
@@ -8,7 +7,19 @@ export function Header({ children }: { children: ReactNode }) {
 }
 
 export function TanstackHeader() {
-  const allCrumbs = useCrumbs();
+  const allCrumbs = useMatches({
+    select: (matches) =>
+      matches
+        .filter(
+          (match): match is typeof match & { loaderData: { crumb: string } } =>
+            !!match.loaderData?.crumb,
+        )
+        .map((match) => ({
+          to: match.pathname,
+          params: match.params,
+          name: match.loaderData.crumb,
+        })),
+  });
   const crumbs = allCrumbs.slice(0, -1);
   const currentCrumb = allCrumbs.at(-1);
   return (
