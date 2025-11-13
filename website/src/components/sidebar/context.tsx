546f8b3430c5c27e80929433a4d92b3f8a3f58f7
Ben Durrant <ben.durrant@marketdojo.com>
2025-11-13 19:26:14 +0000

    persist open state of drawer

--- website/src/components/sidebar/context.tsx
+++ website/src/components/sidebar/context.tsx
@@ -6,9 +6,16 @@ interface SidebarExpandedContext {
   setExpanded: (newValue: boolean) => void;
 }
 
+const storageKey = "benchmarks::sidebar-expanded";
+const breakpoint = 1240;
+
 const getInitialExpanded = createIsomorphicFn()
   .server(() => false)
-  .client(() => window.innerWidth > 1239);
+  .client(() => {
+    const stored = localStorage.getItem(storageKey);
+    if (stored !== null) return stored === "true";
+    return window.innerWidth >= breakpoint;
+  });
 
 // biome-ignore lint/style/useComponentExportOnlyModules: exporting context
 export const SidebarExpandedContext = createContext<SidebarExpandedContext>({
@@ -17,10 +24,13 @@ export const SidebarExpandedContext = createContext<SidebarExpandedContext>({
 });
 
 export function SidebarProvider({ children }: { children: React.ReactNode }) {
-  const [expanded, setExpanded] = useState(getInitialExpanded);
-  useLayoutEffect(() => {
-    setExpanded(getInitialExpanded);
-  }, []);
+  const [expanded, _setExpanded] = useState(getInitialExpanded);
+  function setExpanded(newValue: boolean) {
+    if (typeof window !== "undefined" && window.innerWidth >= breakpoint) {
+      localStorage.setItem(storageKey, newValue.toString());
+    }
+    _setExpanded(newValue);
+  }
   return (
     <SidebarExpandedContext.Provider value={{ expanded, setExpanded }}>
       {children}
