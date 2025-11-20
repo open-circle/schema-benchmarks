8ddeac902f33fcd63b8a182206a4f157ffc002d6
Ben Durrant <ben.durrant@marketdojo.com>
2025-11-20 17:26:22 +0000

    use srvx directly instead of express

--- website/preview.ts
+++ website/preview.ts
@@ -1,33 +1,13 @@
-import express from "express";
-import { toNodeHandler } from "srvx/node";
-
-const PORT = Number.parseInt(process.env.PORT || "3000", 10);
-
-// Create express app
-const app = express();
-
-// Import the prebuilt SSR handler from the production build.
+import { serve } from "srvx";
+import { serveStatic } from "srvx/static";
 // @ts-expect-error untyped
-const { default: handler } = await import("./dist/server/server.js");
+import tanstackHandler from "./dist/server/server.js";
 
-// Convert TanStack Start's fetch-style handler to an Express handler.
-const nodeHandler = toNodeHandler(handler.fetch);
-
-// Serve static assets (JS, CSS, images) from the built client directory.
-app.use(express.static("dist/client"));
-
-// Handle all other requests via SSR.
-// Any request not matched by static assets will be rendered
-// using the TanStack Start server handler.
-app.use(async (req, res, next) => {
-  try {
-    await nodeHandler(req, res);
-  } catch (error) {
-    next(error);
-  }
+const server = serve({
+  fetch: tanstackHandler.fetch,
+  middleware: [serveStatic({ dir: "./dist/client" })],
 });
 
-// Boot server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+await server.ready();
+
+console.log(`Server ready at http://localhost:${server.url}`);
