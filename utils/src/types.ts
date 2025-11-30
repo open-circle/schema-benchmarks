1f5420ca351fecc800cfc3b9e19c8471dca0cd53
Ben Durrant <ben.durrant@marketdojo.com>
2025-11-30 23:57:28 +0000

    make LooseAutocomplete solely a namespace

--- utils/src/types.ts
+++ utils/src/types.ts
@@ -11,33 +11,44 @@ export type WithRequired<T, K extends keyof T> = Compute<
 >;
 
 export namespace LooseAutocomplete {
-  export type String = string & {};
-  // biome-ignore lint/complexity/noBannedTypes: Necessary for autocomplete
-  export type Unknown = {} | null | undefined;
-  export type PropertyKey = String | number | symbol;
+  /**
+   * Accept anything assigable to `string`, but *suggest* using one of the KnownValues.
+   * @example
+   * function parseString<TSchema extends v.GenericSchema<string>>(schema: TSchema, input: LooseAutocomplete.String<v.InferInput<TSchema>>) {}
+   * parseString(v.picklist(["foo", "bar"]), "") // accepts any value without type errors, but suggests "foo" or "bar"
+   */
+  export type String<KnownValues extends string> = KnownValues | (string & {});
+  /**
+   * Accept anything assigable to `unknown`, but *suggest* using one of the KnownValues.
+   * @example
+   * function parse<TSchema extends v.GenericSchema>(schema: TSchema, input: LooseAutocomplete.Unknown<v.InferInput<TSchema>>) {}
+   * parse(v.picklist(["foo", "bar"]), "") // accepts any value without type errors, but suggests "foo" or "bar"
+   */
+  export type Unknown<KnownValues> =
+    | KnownValues
+    // biome-ignore lint/complexity/noBannedTypes: Necessary for autocomplete
+    | {}
+    | null
+    | undefined;
+  /**
+   * Accept anything assigable to `PropertyKey`, but *suggest* using one of the KnownValues.
+   * @example
+   * function hasKey<T extends object>(obj: T, key: LooseAutocomplete.PropertyKey<keyof T>) {}
+   * hasKey({ foo: 1, bar: 2 }, "foo") // accepts any value without type errors, but suggests "foo" or "bar"
+   */
+  export type PropertyKey<KnownValues extends string | number | symbol> =
+    | KnownValues
+    | (string & {})
+    | number
+    | symbol;
 }
-
-/**
- * Accept anything assigable to the BaseType, but *suggest* using one of the KnownValues.
- * @example
- * function parse<TSchema extends v.GenericSchema>(schema: TSchema, input: LooseAutocomplete<v.InferInput<TSchema>, LooseAutocomplete.Unknown>) {}
- * parse(v.picklist(["foo", "bar"]), "") // accepts any value without type errors, but suggests "foo" or "bar"
- *
- * @example
- * function hasKey<T extends object>(obj: T, key: LooseAutocomplete<keyof T, LooseAutocomplete.PropertyKey>) {}
- * hasKey({ foo: 1, bar: 2 }, "foo") // accepts any value without type errors, but suggests "foo" or "bar"
- */
-export type LooseAutocomplete<
-  KnownValues extends BaseType,
-  BaseType = LooseAutocomplete.String,
-> = KnownValues | BaseType;
 
 export type HasRequiredProps<T, True, False> = NonOptionalKeys<T> extends never
   ? False
   : True;
 export type DistributiveOmit<
   T,
-  K extends LooseAutocomplete<keyof T, LooseAutocomplete.PropertyKey>,
+  K extends LooseAutocomplete.PropertyKey<keyof T>,
 > = T extends T ? Omit<T, K> : never;
 
 export type MaybeArray<T> = T | Array<T>;
