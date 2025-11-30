b05558d8e5f148656d11130955035021fd9d1034
Ben Durrant <ben.durrant@marketdojo.com>
2025-11-30 23:49:26 +0000

    rename NonReducible to LooseAutocomplete

--- utils/src/types.ts
+++ utils/src/types.ts
@@ -1,3 +1,5 @@
+import * as v from "valibot";
+
 export type Compute<T> = { [K in keyof T]: T[K] } & unknown;
 export type Satisfies<T extends U, U> = T;
 export type NonOptionalKeys<T> = {
@@ -8,15 +10,26 @@ export type WithRequired<T, K extends keyof T> = Compute<
   Omit<T, K> & Required<Pick<T, K>>
 >;
 
-export namespace NonReducible {
+export namespace LooseAutocomplete {
   export type String = string & {};
   // biome-ignore lint/complexity/noBannedTypes: Necessary for autocomplete
   export type Unknown = {} | null | undefined;
   export type PropertyKey = String | number | symbol;
 }
+
+/**
+ * Accept anything assigable to the BaseType, but *suggest* using one of the KnownValues.
+ * @example
+ * function parse<TSchema extends v.GenericSchema>(schema: TSchema, input: LooseAutocomplete<v.InferInput<TSchema>, LooseAutocomplete.Unknown>) {}
+ * parse(v.picklist(["foo", "bar"]), "") // accepts any value without type errors, but suggests "foo" or "bar"
+ *
+ * @example
+ * function hasKey<T extends object>(obj: T, key: LooseAutocomplete<keyof T, LooseAutocomplete.PropertyKey>) {}
+ * hasKey({ foo: 1, bar: 2 }, "foo") // accepts any value without type errors, but suggests "foo" or "bar"
+ */
 export type LooseAutocomplete<
   KnownValues extends BaseType,
-  BaseType = NonReducible.String,
+  BaseType = LooseAutocomplete.String,
 > = KnownValues | BaseType;
 
 export type HasRequiredProps<T, True, False> = NonOptionalKeys<T> extends never
@@ -24,7 +37,7 @@ export type HasRequiredProps<T, True, False> = NonOptionalKeys<T> extends never
   : True;
 export type DistributiveOmit<
   T,
-  K extends LooseAutocomplete<keyof T, NonReducible.PropertyKey>,
+  K extends LooseAutocomplete<keyof T, LooseAutocomplete.PropertyKey>,
 > = T extends T ? Omit<T, K> : never;
 
 export type MaybeArray<T> = T | Array<T>;
