d8191cdc99c5bc76208f32559e70bc6ff64829cf
Ben Durrant <ben.durrant@marketdojo.com>
2025-12-03 13:10:05 +0000

    basic footer

--- website/src/components/footer/index.tsx
+++ website/src/components/footer/index.tsx
@@ -1,3 +1,10 @@
 export function Footer() {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <span>
+        Created by <a href="https://github.com/EskiMojo14">eskimojo</a> for{" "}
+        <a href="https://github.com/open-circle">Open Circle</a>
+      </span>
+    </footer>
+  );
 }
