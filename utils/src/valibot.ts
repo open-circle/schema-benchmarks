25f147c40c36021c62d62253dc3319233842f656
Ben Durrant <ben.durrant@marketdojo.com>
2025-12-17 15:12:04 +0000

    update valibot

--- utils/src/valibot.ts
+++ utils/src/valibot.ts
@@ -2,9 +2,5 @@ import * as v from "valibot";
 
 export const coerceNumber = v.union([
   v.number(),
-  v.pipe(
-    v.string(),
-    v.transform((value) => Number(value)),
-    v.number(),
-  ),
+  v.pipe(v.string(), v.toNumber()),
 ]);
