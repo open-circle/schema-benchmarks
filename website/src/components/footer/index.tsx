f66ae81d3f28bb6457fb21b03b618fd55c9ac4a1
Ben Durrant <ben.durrant@marketdojo.com>
2026-01-01 11:32:00 +0000

    add Github link to footer

--- website/src/components/footer/index.tsx
+++ website/src/components/footer/index.tsx
@@ -1,6 +1,7 @@
 export function Footer() {
   return (
     <footer className="footer">
+      <a href="https://github.com/open-circle/schema-benchmarks">GitHub</a>
       <span>
         Created by <a href="https://github.com/EskiMojo14">eskimojo</a> for{" "}
         <a href="https://github.com/open-circle">Open Circle</a>
