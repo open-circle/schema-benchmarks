98da0d79544ad1d387368e7033b475d42f9dfe4a
Ben Durrant <ben.durrant@marketdojo.com>
2026-01-16 12:06:38 +0000

    simplify useBreakpoints

--- website/src/hooks/use-breakpoints.ts
+++ website/src/hooks/use-breakpoints.ts
@@ -2,11 +2,11 @@ import { useDebugValue } from "react";
 import { useMediaQuery } from "./use-media-query";
 
 const queries = {
-  phone: "((max-width: 599px))",
-  tabletSmall: "((min-width: 600px) and (max-width: 904px))",
-  tabletLarge: "((min-width: 905px) and (max-width: 1239px))",
-  laptop: "((min-width: 1240px) and (max-width: 1439px))",
-  desktop: "((min-width: 1440px))",
+  phone: "max-width: 599px",
+  tabletSmall: "(min-width: 600px) and (max-width: 904px)",
+  tabletLarge: "(min-width: 905px) and (max-width: 1239px)",
+  laptop: "(min-width: 1240px) and (max-width: 1439px)",
+  desktop: "min-width: 1440px",
 } as const;
 
 export type Breakpoint = keyof typeof queries;
@@ -16,7 +16,7 @@ export function useBreakpoints(
   serverValue = false,
 ) {
   const matches = useMediaQuery(
-    breakpoints.map((breakpoint) => queries[breakpoint]).join(" or "),
+    breakpoints.map((breakpoint) => `(${queries[breakpoint]})`).join(" or "),
     serverValue,
   );
   useDebugValue({ breakpoints, matches });
