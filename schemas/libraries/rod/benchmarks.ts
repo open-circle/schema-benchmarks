52e4342acf73b1d86c5b7f52594382d2bffc86f7
Ben Durrant <ben.durrant@marketdojo.com>
2026-01-27 23:41:36 +0000

    create benchmarks for rod-js

---
+++ schemas/libraries/rod/benchmarks.ts
@@ -0,0 +1,54 @@
+import { defineBenchmarks } from "@schema-benchmarks/schemas";
+import { getVersion } from "@schema-benchmarks/utils/node" with {
+  type: "macro",
+};
+import ts from "dedent" with { type: "macro" };
+import { rod } from "rod-js";
+import { getRodSchema } from ".";
+
+export default defineBenchmarks({
+  library: {
+    name: "rod-js",
+    git: "AxiomCore/rod",
+    optimizeType: "none",
+    version: await getVersion("rod-js"),
+  },
+  createContext: async () => {
+    await rod.init();
+    return { schema: getRodSchema() };
+  },
+  initialization: [
+    {
+      run() {
+        getRodSchema();
+      },
+      snippet: ts`rod.object(...)`,
+    },
+    {
+      run() {
+        rod.init();
+        getRodSchema();
+      },
+      snippet: ts`
+        rod.init();
+        rod.object(...)
+      `,
+    },
+  ],
+  parsing: {
+    allErrors: [
+      {
+        run(data, { schema }) {
+          schema.safeParse(data, { mode: "lazy" });
+        },
+        snippet: ts`schema.safeParse(data, { mode: "lazy" })`,
+      },
+      {
+        run(data, { schema }) {
+          schema.safeParse(data, { mode: "eager" });
+        },
+        snippet: ts`schema.safeParse(data, { mode: "eager" })`,
+      },
+    ],
+  },
+});
