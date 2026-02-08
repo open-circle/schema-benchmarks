2665308d9b0771a0493394d35fd185cf069a184b
Ben Durrant <ben.durrant@marketdojo.com>
2026-02-08 23:27:36 +0000

    move ClientOnly call into plot component

--- website/src/shared/components/plot/index.tsx
+++ website/src/shared/components/plot/index.tsx
@@ -2,16 +2,17 @@ import type * as Plot from "@observablehq/plot";
 import { mergeRefs } from "@schema-benchmarks/utils/react";
 import { ClientOnly } from "@tanstack/react-router";
 import { type FC, useEffect, useRef } from "react";
+import { Spinner } from "../spinner";
 
 export interface PlotContainerProps {
   plot: ReturnType<typeof Plot.plot>;
   ref?: React.Ref<HTMLDivElement>;
 }
 
-export function createPlotComponent<TProps>(
+export function createPlotComponent<TProps extends object>(
   useProps: (props: TProps) => PlotContainerProps,
 ): FC<TProps> {
-  return function PlotComponent(props: TProps) {
+  function PlotComponent(props: TProps) {
     const { plot, ref } = useProps(props);
     const innerRef = useRef<HTMLDivElement>(null);
     useEffect(() => {
@@ -19,12 +20,23 @@ export function createPlotComponent<TProps>(
       return () => plot.remove();
     }, [plot]);
     return (
-      <ClientOnly fallback={<div dir="ltr" className="plot-container" />}>
-        <div
-          dir="ltr"
-          className="plot-container"
-          ref={mergeRefs(ref, innerRef)}
-        />
+      <div
+        dir="ltr"
+        className="plot-container"
+        ref={mergeRefs(ref, innerRef)}
+      />
+    );
+  }
+  return function WrapperComponent(props: TProps) {
+    return (
+      <ClientOnly
+        fallback={
+          <div dir="ltr" className="plot-container">
+            <Spinner size={64} />
+          </div>
+        }
+      >
+        <PlotComponent {...props} />
       </ClientOnly>
     );
   };
