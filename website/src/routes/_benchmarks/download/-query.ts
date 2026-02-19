a1fbecd8348ab059af251eec585520373595f6c6
Ben Durrant <ben.durrant@marketdojo.com>
2026-02-19 12:59:07 +0000

    always return results from website build, as website gets rebuilt with main branch

--- website/src/routes/_benchmarks/download/-query.ts
+++ website/src/routes/_benchmarks/download/-query.ts
@@ -1,15 +1,8 @@
-import {
-  type DownloadResults,
-  downloadResultsSchema,
-  type MinifyType,
-} from "@schema-benchmarks/bench";
+import type { MinifyType } from "@schema-benchmarks/bench";
 import downloadResults from "@schema-benchmarks/bench/download.json" with { type: "json" };
 import { anyAbortSignal } from "@schema-benchmarks/utils";
 import { queryOptions } from "@tanstack/react-query";
 import { createServerFn } from "@tanstack/react-start";
-import { getRequest } from "@tanstack/react-start/server";
-
-import { upfetch } from "#/shared/lib/fetch";
 
 export function getCompiledPath(fileName: string, minify: MinifyType) {
   return fileName
@@ -19,21 +12,7 @@ export function getCompiledPath(fileName: string, minify: MinifyType) {
     .replace(".ts", `/${minify}.js`);
 }
 
-export const getDownloadResultsFn = createServerFn().handler(async () => {
-  let results: DownloadResults | undefined;
-  if (process.env.NODE_ENV === "production") {
-    try {
-      results = await upfetch(
-        "https://raw.githubusercontent.com/open-circle/schema-benchmarks/refs/heads/main/bench/download.json",
-        { signal: getRequest().signal, schema: downloadResultsSchema },
-      );
-    } catch (error) {
-      console.error("Falling back to local results: ", error);
-    }
-  }
-
-  return results ?? downloadResults;
-});
+export const getDownloadResultsFn = createServerFn().handler(() => downloadResults);
 
 export const getDownloadResults = (signalOpt?: AbortSignal) =>
   queryOptions({
