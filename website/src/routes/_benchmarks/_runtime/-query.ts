88346e356232600926e3827e95765ca415cec252
Ben Durrant <ben.durrant@marketdojo.com>
2026-02-19 12:59:07 +0000

    always return results from website build, as website gets rebuilt with main branch

--- website/src/routes/_benchmarks/_runtime/-query.ts
+++ website/src/routes/_benchmarks/_runtime/-query.ts
@@ -1,27 +1,9 @@
-import { type BenchResults, benchResultsSchema } from "@schema-benchmarks/bench";
 import benchResults from "@schema-benchmarks/bench/bench.json" with { type: "json" };
 import { anyAbortSignal } from "@schema-benchmarks/utils";
 import { queryOptions } from "@tanstack/react-query";
 import { createServerFn } from "@tanstack/react-start";
-import { getRequest } from "@tanstack/react-start/server";
 
-import { upfetch } from "#/shared/lib/fetch";
-
-export const getBenchResultsFn = createServerFn().handler(async () => {
-  let results: BenchResults | undefined;
-  if (process.env.NODE_ENV === "production") {
-    try {
-      results = await upfetch(
-        "https://raw.githubusercontent.com/open-circle/schema-benchmarks/refs/heads/main/bench/bench.json",
-        { signal: getRequest().signal, schema: benchResultsSchema },
-      );
-    } catch (error) {
-      console.error("Falling back to local results: ", error);
-    }
-  }
-
-  return results ?? benchResults;
-});
+export const getBenchResultsFn = createServerFn().handler(() => benchResults);
 
 export const getBenchResults = (signalOpt?: AbortSignal) =>
   queryOptions({
