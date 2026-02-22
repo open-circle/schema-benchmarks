0e468ca35ea862eebc3951e7f03834c6f3eefb9a
Ben Durrant <ben.durrant@marketdojo.com>
2026-02-22 21:48:55 +0000

    update bench script to add standard schema testing

--- bench/src/utils/standard.ts
+++
@@ -1,9 +0,0 @@
-import type { MaybePromise } from "@schema-benchmarks/utils";
-import type { StandardSchemaV1 } from "@standard-schema/spec";
-
-export function parse<T extends StandardSchemaV1>(
-  schema: T,
-  data: unknown,
-): MaybePromise<StandardSchemaV1.Result<StandardSchemaV1.InferOutput<T>>> {
-  return schema["~standard"].validate(data);
-}
