dd17fdc35b1739990d1e904883b6a6017835176a
Ben Durrant <ben.durrant@marketdojo.com>
2026-02-24 16:08:11 +0000

    use typia tags system instead of comments

--- schemas/libraries/typia/index.ts
+++ schemas/libraries/typia/index.ts
@@ -1,40 +1,22 @@
 import type { Satisfies } from "@schema-benchmarks/utils";
+import type { tags } from "typia";
 
 import type { ProductData } from "#src";
 
 type ImageSchema = {
   id: number;
   created: Date;
-  /**
-   * @minLength 1
-   * @maxLength 100
-   */
-  title: string;
+  title: string & tags.MinLength<1> & tags.MaxLength<100>;
   type: "jpg" | "png";
   size: number;
-  /**
-   * @format url
-   */
-  url: string;
+  url: string & tags.Format<"url">;
 };
 
 type RatingSchema = {
   id: number;
-  /**
-   * @minimum 1
-   * @maximum 5
-   */
-  stars: number;
-  /**
-   * @minLength 1
-   * @maxLength 100
-   */
-  title: string;
-  /**
-   * @minLength 1
-   * @maxLength 1000
-   */
-  text: string;
+  stars: number & tags.Minimum<1> & tags.Maximum<5>;
+  title: string & tags.MinLength<1> & tags.MaxLength<100>;
+  text: string & tags.MinLength<1> & tags.MaxLength<1000>;
   images: Array<ImageSchema>;
 };
 
@@ -42,41 +24,13 @@ export type TypiaSchema = Satisfies<
   {
     id: number;
     created: Date;
-    /**
-     * @minLength 1
-     * @maxLength 100
-     */
-    title: string;
-    /**
-     * @minLength 1
-     * @maxLength 30
-     */
-    brand: string;
-    /**
-     * @minLength 1
-     * @maxLength 500
-     */
-    description: string;
-    /**
-     * @minimum 1
-     * @maximum 10000
-     */
-    price: number;
-    /**
-     * @minimum 1
-     * @maximum 100
-     */
-    discount: number | null;
-    /**
-     * @minimum 1
-     * @maximum 10
-     */
-    quantity: number;
-    /**
-     * @minItems 1
-     * @maxItems 30
-     */
-    tags: Array<string>;
+    title: string & tags.MinLength<1> & tags.MaxLength<100>;
+    brand: string & tags.MinLength<1> & tags.MaxLength<30>;
+    description: string & tags.MinLength<1> & tags.MaxLength<500>;
+    price: number & tags.Minimum<1> & tags.Maximum<10000>;
+    discount: (number & tags.Minimum<1> & tags.Maximum<100>) | null;
+    quantity: number & tags.Minimum<1> & tags.Maximum<10>;
+    tags: Array<string> & tags.MinItems<1> & tags.MaxItems<30>;
     images: Array<ImageSchema>;
     ratings: Array<RatingSchema>;
   },
