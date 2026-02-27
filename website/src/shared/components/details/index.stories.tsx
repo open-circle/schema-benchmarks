e2832fb2d2b667ee33d760611d4caeb8d119f723
Ben Durrant <ben.durrant@marketdojo.com>
2026-02-27 09:20:57 +0000

    remove unnecessary class

--- website/src/shared/components/details/index.stories.tsx
+++ website/src/shared/components/details/index.stories.tsx
@@ -3,7 +3,7 @@ import preview from "#storybook/preview";
 const meta = preview.meta({
   title: "Components/Details",
   render: () => (
-    <details className="contained">
+    <details>
       <summary>Hello World</summary>
       <p>This is a details element</p>
     </details>
