bfc00beff990eb96972387746b648dfc5bdebbd4
Ben Durrant <ben.durrant@marketdojo.com>
2026-03-03 18:57:26 +0000

    add missing stories

--- website/src/routes/_benchmarks/_runtime/-components/table/index.stories.tsx
+++ website/src/routes/_benchmarks/_runtime/-components/table/index.stories.tsx
@@ -9,15 +9,62 @@ const meta = preview.meta({
   title: "Features/Benchmark/Runtime/Table",
   component: BenchTable,
   args: {
-    results: benchResults.validation.invalid,
-    meanScaler: Bar.getScale(
-      benchResults.validation.invalid.map((r) => r.mean),
-      { lowerBetter: true },
-    ),
-    to: "/validation",
     sortBy: "mean",
     sortDir: "ascending",
   } as const,
 });
 
-export const Default = meta.story();
+export const Initialization = meta.story({
+  args: {
+    results: benchResults.initialization,
+    meanScaler: Bar.getScale(
+      benchResults.initialization.map((r) => r.mean),
+      { lowerBetter: true },
+    ),
+    to: "/initialization",
+  },
+});
+
+export const Validation = meta.story({
+  args: {
+    results: benchResults.validation.valid,
+    meanScaler: Bar.getScale(
+      benchResults.validation.valid.map((r) => r.mean),
+      { lowerBetter: true },
+    ),
+    to: "/validation",
+  },
+});
+
+export const Parsing = meta.story({
+  args: {
+    results: benchResults.parsing.valid,
+    meanScaler: Bar.getScale(
+      benchResults.parsing.valid.map((r) => r.mean),
+      { lowerBetter: true },
+    ),
+    to: "/parsing",
+  },
+});
+
+export const Standard = meta.story({
+  args: {
+    results: benchResults.standard.valid,
+    meanScaler: Bar.getScale(
+      benchResults.standard.valid.map((r) => r.mean),
+      { lowerBetter: true },
+    ),
+    to: "/standard",
+  },
+});
+
+export const String = meta.story({
+  args: {
+    results: benchResults.string["date-time"].valid,
+    meanScaler: Bar.getScale(
+      benchResults.string["date-time"].valid.map((r) => r.mean),
+      { lowerBetter: true },
+    ),
+    to: "/string",
+  },
+});
