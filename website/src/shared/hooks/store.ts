ec27530724e3d5778075b253e3fc0628662b8d45
Ben Durrant <ben.durrant@marketdojo.com>
2026-03-05 17:22:46 +0000

    make subscribe return value disposable

--- website/src/shared/hooks/store.ts
+++ website/src/shared/hooks/store.ts
@@ -1,15 +1,25 @@
-import { ReadonlyStore, Store } from "@tanstack/react-store";
+import { makeDisposable } from "@schema-benchmarks/utils";
+import { Observer, ReadonlyStore, Store } from "@tanstack/react-store";
 import { create, type Draft } from "mutative";
 
 class MutativeStore<T> extends Store<T> {
   updateState(updater: (state: Draft<T>) => void) {
     this.setState(create(updater));
   }
+  override subscribe(observerOrFn: Observer<T> | ((value: T) => void)) {
+    return makeDisposable(super.subscribe(observerOrFn));
+  }
 }
 
-export function createStore<T>(getValue: (prev?: NoInfer<T>) => T): ReadonlyStore<T>;
+class DisposableReadonlyStore<T> extends ReadonlyStore<T> {
+  override subscribe(observerOrFn: Observer<T> | ((value: T) => void)) {
+    return makeDisposable(super.subscribe(observerOrFn));
+  }
+}
+
+export function createStore<T>(getValue: (prev?: NoInfer<T>) => T): DisposableReadonlyStore<T>;
 export function createStore<T>(initialState: T): MutativeStore<T>;
 export function createStore<T>(valueOrFn: T | ((prev?: NoInfer<T>) => T)) {
-  if (typeof valueOrFn === "function") return new ReadonlyStore(valueOrFn);
+  if (typeof valueOrFn === "function") return new DisposableReadonlyStore(valueOrFn);
   return new MutativeStore(valueOrFn);
 }
