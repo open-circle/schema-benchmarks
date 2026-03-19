2ce08414d0bc6e9abae280c95adad56739c1dc31
Ben Durrant <ben.durrant@marketdojo.com>
2026-03-19 10:25:29 +0000

    bump node version to v24 and remove polyfills

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -2,11 +2,6 @@ export type * from "./types.ts";
 
 import type { MaybePromise, PickPartial } from "./types.ts";
 
-// Polyfill because Netlify is using Node v22
-import "@formatjs/intl-durationformat/polyfill.js";
-import "@formatjs/intl-numberformat/polyfill.js";
-import "@formatjs/intl-numberformat/locale-data/en.js";
-
 export function clamp(value: number, min: number, max: number) {
   return Math.min(Math.max(value, min), max);
 }
