71f7a00533a0fc6fec6fcada18691a221e602a23
Ben Durrant <ben.durrant@marketdojo.com>
2026-03-30 15:30:18 +0100

    add string benchmarks

--- schemas/libraries/ata-validator/benchmarks.ts
+++ schemas/libraries/ata-validator/benchmarks.ts
@@ -1,11 +1,21 @@
 import { getVersion } from "@schema-benchmarks/utils/node" with { type: "macro" };
 import type { StandardSchemaV1 } from "@standard-schema/spec";
+import type { FormatName } from "ajv-formats";
+import { Validator } from "ata-validator";
 import ts from "dedent";
 
-import type { ProductData } from "#src";
+import type { ProductData, StringBenchmarkConfig } from "#src";
 import { defineBenchmarks } from "#src";
 
 import { getAtaValidatorSchema } from ".";
+
+const createStringBenchmark = (format: FormatName): StringBenchmarkConfig => ({
+  create() {
+    const schema = new Validator({ type: "string", format } as const);
+    return (testString) => schema.isValidObject(testString);
+  },
+  snippet: ts`{ type: "string", format: "${format}" }`,
+});
 
 const schema = getAtaValidatorSchema();
 
@@ -41,5 +51,16 @@ export default defineBenchmarks({
     allErrors: {
       schema: schema as StandardSchemaV1<unknown, ProductData>, // supports standard schema, but doesn't narrow the type
     },
+  },
+  string: {
+    "date-time": createStringBenchmark("date-time"),
+    date: createStringBenchmark("date"),
+    time: createStringBenchmark("time"),
+    duration: createStringBenchmark("duration"),
+    email: createStringBenchmark("email"),
+    url: createStringBenchmark("url"),
+    uuid: createStringBenchmark("uuid"),
+    ipv4: createStringBenchmark("ipv4"),
+    ipv6: createStringBenchmark("ipv6"),
   },
 });
