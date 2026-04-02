475e1c663724cb015216dfc666246caa1da115a3
Ben Durrant <ben.durrant@marketdojo.com>
2026-04-02 20:28:02 +0100

    install tsdown and use for building schemas

--- schemas/vite.config.ts
+++ schemas/vite.config.ts
@@ -1,37 +1,9 @@
-import * as path from "node:path";
-
 import UnpluginTypia from "@typia/unplugin/vite";
 import macros from "unplugin-macros/vite";
-import dts from "vite-plugin-dts";
 import { defineConfig } from "vitest/config";
 
-import { dependencies } from "./package.json";
-
 export default defineConfig({
-  build: {
-    lib: {
-      entry: "libraries/index.ts",
-      formats: ["es"],
-      fileName: "index",
-    },
-    target: ["node24", "esnext"],
-    sourcemap: true,
-    rollupOptions: {
-      external: [
-        /^node:/,
-        ...Object.keys(dependencies).flatMap((dep) => [dep, new RegExp(`^${dep}/`)]),
-      ],
-    },
-  },
-  resolve: {
-    alias: {
-      "#src": path.resolve(process.cwd(), "./src/index.ts"),
-    },
-  },
-  define: {
-    self: "globalThis",
-  },
-  plugins: [UnpluginTypia({ log: false }), macros(), dts({ rollupTypes: true })],
+  plugins: [UnpluginTypia({ log: false }), macros()],
   test: {
     include: ["**/*.node.test.ts"], // not tsx - if you're using React, test in the browser
   },
