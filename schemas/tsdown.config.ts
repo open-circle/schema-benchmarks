144f780b490ace3d810ee7aad25a516e32056ef1
Ben Durrant <ben.durrant@marketdojo.com>
2026-04-02 20:28:02 +0100

    install tsdown and use for building schemas

---
+++ schemas/tsdown.config.ts
@@ -0,0 +1,20 @@
+import * as path from "node:path";
+
+import UnpluginTypia from "@typia/unplugin/rolldown";
+import { defineConfig } from "tsdown";
+import macros from "unplugin-macros/rolldown";
+
+export default defineConfig({
+  entry: ["libraries/index.ts"],
+  format: "esm",
+  target: ["node24", "esnext"],
+  sourcemap: true,
+  dts: true,
+  alias: {
+    "#src": path.resolve(process.cwd(), "./src/index.ts"),
+  },
+  plugins: [UnpluginTypia({ log: false }), macros()],
+  deps: {
+    neverBundle: [/node:/],
+  },
+});
