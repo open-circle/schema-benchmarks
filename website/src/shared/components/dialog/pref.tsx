1392f2378323ffea54fe67954818a55ca52f669f
Ben Durrant <ben.durrant@marketdojo.com>
2026-04-28 01:18:01 +0100

    add preference buttons to dialog

--- website/src/shared/components/dialog/pref.tsx
+++ website/src/shared/components/dialog/pref.tsx
@@ -1,3 +1,19 @@
+import { unsafeFromEntries } from "@schema-benchmarks/utils";
+import { ReactNode } from "react";
+import bem from "react-bem-helper";
+
+import { Button, ButtonGroup } from "#/shared/components/button";
+import { useNpmSite, useStyle, useTheme } from "#/shared/components/prefs/context";
+import { MdSymbol } from "#/shared/components/symbol";
+import { useIdDefault } from "#/shared/hooks/use-id-default";
+import {
+  npmSiteSchema,
+  styleLabels,
+  styleSchema,
+  themeLabels,
+  themeSchema,
+} from "#/shared/lib/prefs/constants";
+
 import { Dialog, DialogContent, DialogTitle } from ".";
 
 export interface PreferencesDialogProps {
@@ -5,12 +21,79 @@ export interface PreferencesDialogProps {
   onClose?: () => void;
 }
 
+const cls = bem("pref-dialog");
+
 export function PreferencesDialog({ open, onClose }: PreferencesDialogProps) {
+  const { style, setStyle } = useStyle();
+  const { theme, setTheme } = useTheme();
+  const { npmSite, setNpmSite } = useNpmSite();
   return (
-    <Dialog {...{ open, onClose }} aria-labelledby="pref-dialog-title" closedby="any">
+    <Dialog {...{ open, onClose }} aria-labelledby="pref-dialog-title" closedby="any" {...cls()}>
       <DialogContent>
         <DialogTitle id="pref-dialog-title">Preferences</DialogTitle>
+        <div {...cls("prefs")}>
+          <PrefGroup
+            options={styleSchema.options}
+            labels={styleLabels}
+            title="Style"
+            selected={style}
+            onChange={setStyle}
+          />
+          <PrefGroup
+            options={themeSchema.options}
+            labels={themeLabels}
+            title="Theme"
+            selected={theme}
+            onChange={setTheme}
+          />
+          <PrefGroup
+            options={npmSiteSchema.options}
+            labels={unsafeFromEntries(
+              npmSiteSchema.options.map((opt) => [opt, { label: opt, icon: "deployed_code" }]),
+            )}
+            title="NPM browser"
+            selected={npmSite}
+            onChange={setNpmSite}
+          />
+        </div>
       </DialogContent>
     </Dialog>
+  );
+}
+
+interface PrefGroupProps<TOpt extends string> {
+  title: ReactNode;
+  titleId?: string;
+  options: ReadonlyArray<TOpt>;
+  labels: Record<TOpt, { icon: string; label: ReactNode }>;
+  selected: TOpt;
+  onChange: (opt: TOpt) => void;
+}
+
+function PrefGroup<TOpt extends string>({
+  title,
+  titleId: titleIdProp,
+  options,
+  labels,
+  selected,
+  onChange,
+}: PrefGroupProps<TOpt>) {
+  const titleId = useIdDefault(titleIdProp);
+  return (
+    <div {...cls("group")}>
+      <h5 id={titleId}>{title}</h5>
+      <ButtonGroup ariaLabelledBy={titleId} variant="outlined">
+        {options.map((opt) => (
+          <Button
+            key={opt}
+            icon={<MdSymbol>{labels[opt].icon}</MdSymbol>}
+            aria-pressed={opt === selected}
+            onClick={() => onChange(opt)}
+          >
+            {labels[opt].label}
+          </Button>
+        ))}
+      </ButtonGroup>
+    </div>
   );
 }
--- website/src/shared/lib/prefs/constants.ts
+++ website/src/shared/lib/prefs/constants.ts
@@ -3,17 +3,17 @@ import * as v from "valibot";
 export const themeSchema = v.fallback(v.picklist(["system", "light", "dark"]), "system");
 export type Theme = v.InferOutput<typeof themeSchema>;
 export const themeLabels: Record<Theme, { label: string; icon: string }> = {
-  light: { label: "Light theme", icon: "light_mode" },
-  dark: { label: "Dark theme", icon: "dark_mode" },
-  system: { label: "System theme", icon: "routine" },
+  light: { label: "Light", icon: "light_mode" },
+  dark: { label: "Dark", icon: "dark_mode" },
+  system: { label: "System", icon: "routine" },
 };
 
 export const styleSchema = v.fallback(v.picklist(["code", "normal"]), "code");
 export type Style = v.InferOutput<typeof styleSchema>;
 export const styleLabels: Record<Style, { label: string; icon: string }> = {
-  code: { label: "Code style", icon: "code" },
-  normal: { label: "Normal style", icon: "match_case" },
+  code: { label: "Code", icon: "code" },
+  normal: { label: "Normal", icon: "match_case" },
 };
 
-export const npmSiteSchema = v.fallback(v.picklist(["npmjs.com", "npmx.dev"]), "npmx.dev");
+export const npmSiteSchema = v.fallback(v.picklist(["npmx.dev", "npmjs.com"]), "npmx.dev");
 export type NpmSite = v.InferOutput<typeof npmSiteSchema>;
