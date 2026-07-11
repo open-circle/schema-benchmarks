5a2bef08c46d063532ccacf37af6c79ca9503adf
Ben Durrant <ben.durrant@marketdojo.com>
2026-07-11 19:39:39 +0100

    replace @typia/unplugin with @ttsc/unplugin and update related code

--- schemas/vite.config.ts
+++ schemas/vite.config.ts
@@ -1,9 +1,9 @@
-import UnpluginTypia from "@typia/unplugin/vite";
+import ttsc from "@ttsc/unplugin/vite";
 import macros from "unplugin-macros/vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [UnpluginTypia({ log: false }), macros()],
+  plugins: [ttsc(), macros()],
   test: {
     include: ["**/*.node.test.ts"], // not tsx - if you're using React, test in the browser
   },
