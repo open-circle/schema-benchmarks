8c8185fbac2e11eb25e29395d6f08492cfadbd92
Ben Durrant <ben.durrant@marketdojo.com>
2026-07-21 13:03:10 +0100

    allow providing custom date formatters and date ranges to timeline items

--- website/src/shared/hooks/format/use-date-formatter.ts
+++ website/src/shared/hooks/format/use-date-formatter.ts
@@ -1,16 +1,21 @@
-import { useCallback } from "react";
+import { longDateFormatter } from "@schema-benchmarks/utils";
 
 import { useStyle } from "#src/shared/components/prefs/context";
 
-export function useDateFormatter(formatter: Intl.DateTimeFormat) {
+export interface Formatters {
+  normal?: Intl.DateTimeFormat;
+  code?: (date: Date) => string;
+}
+
+export function useDateFormatter({
+  normal: formatter = longDateFormatter,
+  code: codeFormatter = (date) => date.toISOString().split("T")[0]!,
+}: Formatters = {}) {
   const { style } = useStyle();
-  return useCallback(
-    (date: Date) => {
-      if (style === "code") {
-        return date.toISOString().split("T")[0];
-      }
-      return formatter.format(date);
-    },
-    [style, formatter],
-  );
+  return (date: Date) => {
+    if (style === "code") {
+      return codeFormatter(date);
+    }
+    return formatter.format(date);
+  };
 }
