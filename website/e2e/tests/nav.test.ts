f4c09cb92e6f9ccd314b8686723bd6e1417382d6
Ben Durrant <ben.durrant@marketdojo.com>
2026-07-21 23:31:17 +0100

    condense navigation link tests into a single test for efficiency

--- website/e2e/tests/nav.test.ts
+++ website/e2e/tests/nav.test.ts
@@ -8,27 +8,27 @@ test("homepage is selected", async ({ page, sidebar }) => {
   await expect(sidebar.getLinkByName("Home")).toBeCurrent("page");
 });
 
-for (const [name, path] of [
-  ["Download", "/download"],
-  ["Initialization", "/initialization"],
-  ["Validation", "/validation"],
-  ["Parsing", "/parsing"],
-  ["Codec", "/codec"],
-  ["Standard Schema", "/standard"],
-  ["String", "/string"],
-  ["Stack", "/stack"],
-  ["Libraries", "/libraries"],
-  ["Blog", "/blog"],
-] as const) {
-  test(`navigation link: ${name}`, async ({ page, sidebar }) => {
-    await page.goto("/");
+test("navigation links work", async ({ page, sidebar }) => {
+  await page.goto("/");
 
-    await sidebar.open();
+  await sidebar.open();
 
+  for (const [name, path] of [
+    ["Download", "/download"],
+    ["Initialization", "/initialization"],
+    ["Validation", "/validation"],
+    ["Parsing", "/parsing"],
+    ["Codec", "/codec"],
+    ["Standard Schema", "/standard"],
+    ["String", "/string"],
+    ["Stack", "/stack"],
+    ["Libraries", "/libraries"],
+    ["Blog", "/blog"],
+  ] as const) {
     const link = await sidebar.selectLinkByName(name);
 
     await expect(page).toHaveURL((url) => url.pathname === path);
 
     await expect(link).toBeCurrent("page");
-  });
-}
+  }
+});
