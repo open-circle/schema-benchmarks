1b1bea8b31cace204e7e80f09288e0994d01d0b7
Ben Durrant <ben.durrant@marketdojo.com>
2026-07-22 02:26:35 +0100

    add utility for matching breakpoints in e2e tests

--- website/src/shared/hooks/use-breakpoints.ts
+++ website/src/shared/hooks/use-breakpoints.ts
@@ -2,7 +2,7 @@ import { useDebugValue } from "react";
 
 import { useMediaQuery } from "./use-media-query";
 
-const queries = {
+export const breakpointQueries = {
   phone: "max-width: 599px",
   tabletSmall: "(min-width: 600px) and (max-width: 904px)",
   tabletLarge: "(min-width: 905px) and (max-width: 1239px)",
@@ -10,11 +10,11 @@ const queries = {
   desktop: "min-width: 1440px",
 } as const;
 
-export type Breakpoint = keyof typeof queries;
+export type Breakpoint = keyof typeof breakpointQueries;
 
 export function useBreakpoints(breakpoints: Array<Breakpoint>, serverValue = false) {
   const matches = useMediaQuery(
-    breakpoints.map((breakpoint) => `(${queries[breakpoint]})`).join(" or "),
+    breakpoints.map((breakpoint) => `(${breakpointQueries[breakpoint]})`).join(" or "),
     serverValue,
   );
   useDebugValue({ breakpoints, matches });
