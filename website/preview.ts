4fee558806b4a1abcc5298ebcf829f6de250ea0e
Ben Durrant <ben.durrant@marketdojo.com>
2026-07-23 11:36:26 +0100

    fix import in preview.ts

--- website/preview.ts
+++ website/preview.ts
@@ -1,14 +1,14 @@
 import { serve } from "srvx";
-import { serveStatic } from "srvx/static";
+import { staticMiddleware } from "srvx/static";
 
 // @ts-expect-error untyped
 import tanstackHandler from "./dist/server/server.js";
 
 const server = serve({
   fetch: tanstackHandler.fetch,
-  middleware: [serveStatic({ dir: "./dist/client" })],
+  middleware: [staticMiddleware({ dir: "./dist/client" })],
 });
 
 await server.ready();
 
-console.log(`Server ready at http://localhost:${server.url}`);
+console.log(`Server ready at ${server.url}`);
