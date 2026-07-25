20c8fb695b8cea15a54cdf6664738d5a3916bb0e
Ben Durrant <ben.durrant@marketdojo.com>
2026-07-25 12:39:58 +0100

    remove registry-url from setup-node action configuration, and no longer error on stderr in getVersion function

--- utils/src/node.ts
+++ utils/src/node.ts
@@ -24,8 +24,7 @@ const versionCache = new Map<string, string>();
 
 export async function getVersion(libraryName: string) {
   if (versionCache.has(libraryName)) return versionCache.get(libraryName)!;
-  const { stdout, stderr } = await exec(`pnpm --filter schemas list ${libraryName} --json`);
-  if (stderr) throw new Error(stderr);
+  const { stdout } = await exec(`pnpm --filter schemas list ${libraryName} --json`);
   const data = v.parse(pnpmListSchema, stdout);
   const dep = data[0]?.dependencies[libraryName];
   if (!dep) throw new Error(`No version found for ${libraryName}`);
