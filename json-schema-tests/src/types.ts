b622c1614ce19ad5bdc5bf7e75c49d25fd88e984
Ben Durrant <ben.durrant@marketdojo.com>
2026-08-01 00:38:01 +0100

    create json-schema-tests package with types and checkout script

---
+++ json-schema-tests/src/types.ts
@@ -0,0 +1,19 @@
+import * as v from "valibot";
+
+import { targets } from "./constants.ts";
+
+const testCaseSchema = v.object({
+  description: v.string(),
+  schema: v.looseObject({}),
+  tests: v.array(
+    v.object({
+      description: v.string(),
+      data: v.any(),
+      valid: v.boolean(),
+    }),
+  ),
+});
+export type TestCase = v.InferOutput<typeof testCaseSchema>;
+
+export const targetSchema = v.picklist(targets);
+export type Target = v.InferOutput<typeof targetSchema>;
