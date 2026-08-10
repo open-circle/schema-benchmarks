e0e04ffc1de4e9279fea9ee40611ed0ab3493433
Ben Durrant <ben.durrant@marketdojo.com>
2026-08-10 15:18:14 +0100

    make sure each tab has content

--- website/src/routes/json-schema/compliance/-content/index.ts
+++ website/src/routes/json-schema/compliance/-content/index.ts
@@ -1,3 +1,9 @@
-export { default as roundtrip } from "./roundtrip.mdx";
-export { default as semantics } from "./semantics.mdx";
-export { default as validation } from "./validation.mdx";
+import type { ComplianceType } from "@schema-benchmarks/schemas";
+import { promiseAllKeyed } from "@schema-benchmarks/utils";
+import type { MDXContent } from "mdx/types";
+
+export const content: Record<ComplianceType, MDXContent> = await promiseAllKeyed({
+  validation: import("./validation.mdx").then((mod) => mod.default),
+  roundtrip: import("./roundtrip.mdx").then((mod) => mod.default),
+  semantics: import("./semantics.mdx").then((mod) => mod.default),
+});
