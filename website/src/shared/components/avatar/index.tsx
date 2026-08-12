801cf80e3fc4fbd9c42fd627b13c46b058c88e8b
Ben Durrant <ben.durrant@marketdojo.com>
2026-08-12 17:54:08 +0100

    make author avatars link to github profile in blog

--- website/src/shared/components/avatar/index.tsx
+++ website/src/shared/components/avatar/index.tsx
@@ -1,6 +1,7 @@
 import type { ComponentPropsWithRef } from "react";
 import bem from "react-bem-helper";
 
+import type { RichTooltipProps } from "#src/shared/components/tooltip";
 import { withTooltip } from "#src/shared/components/tooltip";
 import { useLoadImage } from "#src/shared/hooks/use-load-image";
 
@@ -15,22 +16,38 @@ export interface AvatarProps {
 
 const cls = bem("avatar");
 
-const TooltipDiv = withTooltip("div");
-const TooltipLi = withTooltip("li");
-
-export function Avatar({ src, label, size = "md", "~as": asProp }: AvatarProps) {
+export function Avatar({ src, label, size = "md", "~as": asProp = "div" }: AvatarProps) {
   const loadState = useLoadImage(src);
-  const Component = asProp === "li" ? TooltipLi : TooltipDiv;
+  const Component = asProp;
   return (
-    <Component tooltip={label} {...cls({ modifiers: size })}>
+    <Component {...cls({ modifiers: size })}>
       {loadState === "loaded" ? (
-        <img src={src} alt={label} {...cls("image")} />
+        <img src={src} alt="" {...cls("image")} />
       ) : (
         <div {...cls("initial")}>{label.charAt(0).toUpperCase()}</div>
       )}
     </Component>
   );
 }
+
+interface AvatarLinkProps
+  extends Omit<AvatarProps, "label">, Omit<ComponentPropsWithRef<"a">, "children"> {
+  tooltip: string | RichTooltipProps;
+}
+
+export const AvatarExternalLink = withTooltip(
+  function AvatarExternalLink({ src, size, "~as": asProp, tooltip, ...props }: AvatarLinkProps) {
+    const label = typeof tooltip === "string" ? tooltip : (tooltip.subhead ?? "");
+    return (
+      <a className="avatar-link" {...props}>
+        <Avatar {...{ src, size, label, "~as": asProp }} />
+      </a>
+    );
+  },
+  {
+    required: true,
+  },
+);
 
 export interface AvatarListProps extends ComponentPropsWithRef<"ul"> {
   items: Array<Omit<AvatarProps, "~as" | "size">>;
@@ -47,6 +64,32 @@ export function AvatarList({ items, size = "md", ...props }: AvatarListProps) {
         .toReversed()
         .map((item) => (
           <Avatar key={item.label} {...item} size={size} {...{ "~as": "li" }} />
+        ))}
+    </ul>
+  );
+}
+
+export interface AvatarLinkListProps extends ComponentPropsWithRef<"ul"> {
+  items: Array<Omit<AvatarLinkProps, "~as" | "size">>;
+  size?: AvatarSize;
+}
+
+export function AvatarLinkList({ items, size = "md", ...props }: AvatarLinkListProps) {
+  return (
+    <ul {...listCls({ modifiers: size })} {...props}>
+      {items
+        .slice()
+        .toReversed()
+        .map((item) => (
+          <AvatarExternalLink
+            key={
+              item.href ??
+              (typeof item.tooltip === "string" ? item.tooltip : (item.tooltip.subhead ?? ""))
+            }
+            {...item}
+            size={size}
+            {...{ "~as": "li" }}
+          />
         ))}
     </ul>
   );
