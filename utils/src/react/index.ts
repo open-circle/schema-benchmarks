0ff9779000d13f6074d29e3eda2cdfd20f273692
Ben Durrant <ben.durrant@marketdojo.com>
2026-08-15 01:09:18 +0100

    support cleanup functions in mergeRefs

--- utils/src/react/index.ts
+++ utils/src/react/index.ts
@@ -3,11 +3,20 @@ import type { Ref, RefCallback } from "react";
 export const mergeRefs =
   <T>(...refs: Array<Ref<T> | undefined>): RefCallback<T> =>
   (value) => {
+    const cleanups: Array<() => void> = [];
     for (const ref of refs) {
       if (typeof ref === "function") {
-        ref(value);
+        const cleanup = ref(value);
+        if (typeof cleanup === "function") {
+          cleanups.push(cleanup);
+        }
       } else if (ref) {
         ref.current = value;
       }
     }
+    return () => {
+      for (const cleanup of cleanups) {
+        cleanup();
+      }
+    };
   };
