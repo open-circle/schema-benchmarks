56f563299426609b35b259159ca5c08ab429f4fd
EskiMojo14 <ben.j.durrant@gmail.com>
2026-08-19 22:59:13 +0100

    mock external download APIs in tests

--- website/e2e/fixtures/index.ts
+++ website/e2e/fixtures/index.ts
@@ -41,6 +41,16 @@ export const test = baseTest.extend<POMFixtures & UtilFixtures>({
   fontsLoaded: async ({ page }, use) => use(() => waitForFontsLoaded(page)),
 });
 
+test.beforeEach("Mock external download APIs", async ({ context }) => {
+  await context.route("https://api.npmjs.org/downloads/point/last-week/**", (route) =>
+    route.fulfill({ json: { downloads: 0 } }),
+  );
+
+  await context.route("https://api.jsr.io/scopes/**/downloads", (route) =>
+    route.fulfill({ json: { total: [] } }),
+  );
+});
+
 function createAttributeMatcher<ExpectedValue extends string | undefined>(
   assertionName: string,
   attributeName: string,
