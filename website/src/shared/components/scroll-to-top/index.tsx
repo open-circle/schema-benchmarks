dbbf2bcfb649433af952795ed9b3703e8f59709b
Ben Durrant <ben.durrant@marketdojo.com>
2026-08-20 15:01:20 +0100

    wrap setScrollContainer for stability

--- website/src/shared/components/scroll-to-top/index.tsx
+++ website/src/shared/components/scroll-to-top/index.tsx
@@ -1,6 +1,6 @@
 import { environmentManager } from "@tanstack/react-query";
 import { radEventListeners } from "rad-event-listeners";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import bem from "react-bem-helper";
 
 import { FloatingActionButton } from "#src/shared/components/button/floating";
@@ -12,10 +12,13 @@ export function useScrolled({ threshold = 100 } = {}) {
   const [scrollContainer, setScrollContainerState] = useState<HTMLElement | null>(null);
   const [scrolled, setScrolled] = useState(false);
 
-  function setScrollContainer(container: HTMLElement | null) {
-    setScrollContainerState(container);
-    setScrolled(container ? container.scrollTop > threshold : false);
-  }
+  const setScrollContainer = useCallback(
+    function setScrollContainer(container: HTMLElement | null) {
+      setScrollContainerState(container);
+      setScrolled(container ? container.scrollTop > threshold : false);
+    },
+    [threshold],
+  );
 
   useEffect(() => {
     if (!scrollContainer) return;
