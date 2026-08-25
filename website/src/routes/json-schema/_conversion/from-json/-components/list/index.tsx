f402cda77ee2b8203df8127caf70e8814a1bea3d
Ben Durrant <ben.durrant@marketdojo.com>
2026-08-25 17:06:43 +0100

    convert json to schema results from cards to list for mobile view

---
+++ website/src/routes/json-schema/_conversion/from-json/-components/list/index.tsx
@@ -0,0 +1,73 @@
+import type { SchemaFromJsonResult } from "@schema-benchmarks/bench";
+import { formatDuration, getTransitionName } from "@schema-benchmarks/utils";
+import bem from "react-bem-helper";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { DownloadCount } from "#src/routes/_benchmarks/-components/count";
+import { CodeBlock } from "#src/shared/components/code";
+import { List, ListItem, ListItemContent } from "#src/shared/components/list";
+import { Bar } from "#src/shared/components/table/bar";
+
+export interface FromJsonListProps {
+  results: Array<SchemaFromJsonResult>;
+  meanScaler: ReturnType<typeof Bar.getScale>;
+}
+
+const cls = bem("json-schema-list");
+
+export function FromJsonList({ results, meanScaler }: FromJsonListProps) {
+  return (
+    <List aria-label="Results" {...cls()}>
+      {results.map((result) => {
+        const id = getTransitionName("json-schema-list-item", {
+          libraryName: result.libraryName,
+          note: result.note,
+        });
+
+        return (
+          <ListItem key={result.id} id={id} style={{ viewTransitionName: id }}>
+            <details {...cls("item")}>
+              <summary {...cls("summary")}>
+                <ListItemContent
+                  lines={3}
+                  overline={<code className="language-text">{result.version}</code>}
+                  primary={
+                    <>
+                      <code className="language-text">{result.libraryName}</code>
+                      {result.note ? ` (${result.note})` : null}
+                    </>
+                  }
+                  supporting={formatDuration(result.mean)}
+                  trailing={
+                    <span {...cls("bar")}>
+                      <Bar {...meanScaler(result.mean)} />
+                    </span>
+                  }
+                />
+              </summary>
+              <div {...cls("details")}>
+                <div {...cls("code")}>
+                  <CodeBlock>{result.snippet}</CodeBlock>
+                </div>
+                <dl {...cls("metrics")}>
+                  <div>
+                    <dt>Mean</dt>
+                    <dd>{formatDuration(result.mean)}</dd>
+                  </div>
+                  <div>
+                    <dt>Downloads / wk</dt>
+                    <dd>
+                      <ErrorBoundary fallback={null}>
+                        <DownloadCount libraryName={result.libraryName} useLink={false} />
+                      </ErrorBoundary>
+                    </dd>
+                  </div>
+                </dl>
+              </div>
+            </details>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
