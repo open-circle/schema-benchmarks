98eb7bab50cf93610d4036c9237a2229c58423ad
EskiMojo14 <18308300+EskiMojo14@users.noreply.github.com>
2026-08-31 16:45:05 +0000

    chore: bump JSON Schema test-suite SHA

--- json-schema-tests/constants/sha.gen.ts
+++ json-schema-tests/constants/sha.gen.ts
@@ -1,3 +1,3 @@
 // Pinned upstream revision for json-schema-org/json-schema-test-suite.
 // Update this with \`pnpm json-schema-tests:bump\`. Optionally provide a commit SHA to set a specific commit, e.g. \`pnpm json-schema-tests:bump -c <sha>\`.
-export default "b01af8c8d50244a2eb4dd3e01073e24823aa8691"; // test(iri-reference): add a trailing newline as invalid (#1144)
+export default "3c25e5f709192aadf67cf7f2eb19771a57131fec"; // test(idn-hostname): add UTS 46 mapping-step cases (#1112)
