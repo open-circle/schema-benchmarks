2519b635eced1170196d1a5b205e2dd3d79488ae
Ben Durrant <ben.durrant@marketdojo.com>
2026-09-04 10:58:55 +0100

    remove renderHook extension (already done by vitest-browser-react)

--- website/test/browser/setup.ts
+++ website/test/browser/setup.ts
@@ -1,17 +1,12 @@
 import "#src/shared/styles/index.css";
-import { renderHook } from "vitest-browser-react";
 import { page } from "vitest/browser";
 
 import { renderWithProviders } from "./render";
 
-page.extend({
-  renderWithProviders,
-  renderHook,
-});
+page.extend({ renderWithProviders });
 
 declare module "vitest/browser" {
   interface BrowserPage {
     renderWithProviders: typeof renderWithProviders;
-    renderHook: typeof renderHook;
   }
 }
