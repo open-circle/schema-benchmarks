156d4a410a3074384688aeb941bb3ab53cba0c4d
Ben Durrant <ben.durrant@marketdojo.com>
2026-09-04 12:00:04 +0100

    test cleanup callbacks for mergeRefs

--- utils/src/react/index.node.test.ts
+++ utils/src/react/index.node.test.ts
@@ -6,12 +6,17 @@ import { mergeRefs } from "./index.ts";
 describe("mergeRefs", () => {
   it("should call all refs", () => {
     const refObject: RefObject<string | null> = { current: null };
+    const cleanupFn = vi.fn();
     const refCallback = vi.fn<RefCallback<string>>();
-    const merged = mergeRefs(refObject, refCallback);
+    const refWithCleanup = vi.fn<RefCallback<string>>(() => cleanupFn);
+    const merged = mergeRefs(refObject, refCallback, refWithCleanup, null);
 
     const value = "foo";
-    merged(value);
+    const cleanup = merged(value);
     expect(refObject.current).toBe(value);
     expect(refCallback).toHaveBeenCalledWith(value);
+    expect(refWithCleanup).toHaveBeenCalledWith(value);
+    cleanup();
+    expect(cleanupFn).toHaveBeenCalled();
   });
 });
