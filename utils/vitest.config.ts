6175797cd12b1e95a23804ebdabce011a2ea3df0
Ben Durrant <ben.durrant@marketdojo.com>
2026-09-04 11:59:33 +0100

    enable coverage collection for tests

--- utils/vitest.config.ts
+++ utils/vitest.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+      reportsDirectory: "./coverage",
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+    },
     projects: [
       {
         test: {
