a33471e81824a6c8a46d6982917be780813e995d
EskiMojo14 <18308300+EskiMojo14@users.noreply.github.com>
2026-09-14 15:42:05 +0000

    chore: bump JSON Schema test-suite SHA

--- json-schema-tests/constants/sha.gen.ts
+++ json-schema-tests/constants/sha.gen.ts
@@ -1,3 +1,3 @@
 // Pinned upstream revision for json-schema-org/json-schema-test-suite.
 // Update this with \`pnpm json-schema-tests:bump\`. Optionally provide a commit SHA to set a specific commit, e.g. \`pnpm json-schema-tests:bump -c <sha>\`.
-export default "f6fd52a0a95472e079cbfc6ef7f089702b80e045"; // test(uri-template): reject malformed percent-encoding (#1179)
+export default "1d82f70b87bba8c6f5d47b6b7e256401215d635b"; // Remove IPvFuture tests (#1176)
