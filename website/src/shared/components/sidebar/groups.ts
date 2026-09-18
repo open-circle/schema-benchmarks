6066c3a21d13da5958ea22bbb38dfda8740750e9
Ben Durrant <ben.durrant@marketdojo.com>
2026-09-18 17:47:07 +0100

    change sidebar icon

--- website/src/shared/components/sidebar/groups.ts
+++ website/src/shared/components/sidebar/groups.ts
@@ -61,7 +61,7 @@ export const sidebarGroups: Array<SidebarGroup> = [
     key: "post-runtime",
     links: [
       { ...linkOptions({ to: "/stack" }), name: "Stack", icon: "error" },
-      { ...linkOptions({ to: "/typescript" }), name: "TypeScript", icon: "code_blocks" },
+      { ...linkOptions({ to: "/typescript" }), name: "TypeScript", icon: "assignment_turned_in" },
     ],
   },
   {
